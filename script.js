2503ffda1b5471706760f88697bf6b6a9ef26ed4
NeoTime <riglerjeff@gmail.com>
2018-01-15 09:56:08 -0800

    Logic JavaScript

---
+++ script.js
@@ -0,0 +1,14 @@
+var date = new Date()
+document.body.innerHTML = "<h1>Today is: " + date + "</h1>";
+
+var a = 5;
+var b = 4;
+var theNumbersMatch
+
+if ( a == b ) {
+    theNumbersMatch = true;
+} else {
+    theNumbersMatch = false;
+}
+
+console.log("The numbers match: " + theNumbersMatch);
